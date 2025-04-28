c99b003ce0a5eaffed1c39f8eb22786e0b588ccd
Josef Boukdir <josef@boukdir.se>
2025-04-28 20:57:52 +0200

    removed old migration and added new ones

--- backend/migrations/000001_create_permissions_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS permissions (
-    id bigserial PRIMARY KEY,
-    name text NOT NULL,
-    description text
-);
--- backend/migrations/000002_create_roles_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS roles (
-    id bigserial PRIMARY KEY,
-    name text NOT NULL UNIQUE,
-    description text
-);
